75461f9f6b899fbf01038e3facef1375beb5e103
Sanghyeok Park <suran5281@gmail.com>
2022-11-21 10:34:59 +0900

    상품 이미지 업로드

--- BookStore/src/main/java/com/psh/controller/AdminController.java
+++ BookStore/src/main/java/com/psh/controller/AdminController.java
@@ -1,26 +1,43 @@
 package com.psh.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.psh.model.AttachImageVO;
 import com.psh.model.AuthorVO;
 import com.psh.model.BookVO;
 import com.psh.model.Criteria;
 import com.psh.model.PageDTO;
 import com.psh.service.AdminService;
 import com.psh.service.AuthorService;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 @Controller
 @RequestMapping("/admin")
@@ -253,5 +270,133 @@ public class AdminController {
 		
 		return "redirect:/admin/authorManage";
 		
-	}	
+	}
+	
+	/* 첨부 파일 업로드 */
+	@PostMapping(value="/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<List<AttachImageVO>> uploadAjaxActionPOST(MultipartFile[] uploadFile) {
+		
+		logger.info("uploadAjaxActionPOST..........");
+		
+		/* 이미지 파일 체크 */
+		for(MultipartFile multipartFile: uploadFile) {
+			
+			File checkfile = new File(multipartFile.getOriginalFilename());
+			String type = null;
+			
+			try {
+				type = Files.probeContentType(checkfile.toPath());
+				logger.info("MIME TYPE : " + type);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			if(!type.startsWith("image")) {
+				
+				List<AttachImageVO> list = null;
+				return new ResponseEntity<>(list, HttpStatus.BAD_REQUEST);
+				
+			}
+			
+		}// for
+		
+		String uploadFolder = "C:\\upload";
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		String datePath = str.replace("-", File.separator);
+		
+		/* 폴더 생성 */
+		File uploadPath = new File(uploadFolder, datePath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		
+		}
+		
+		/* 이미저 정보 담는 객체 */
+		List<AttachImageVO> list = new ArrayList();
+
+				
+		//향상된 for
+		for(MultipartFile multipartFile : uploadFile) {
+			
+			/* 이미지 정보 객체 */
+			AttachImageVO vo = new AttachImageVO();
+			
+			/* 파일 이름 */
+			String uploadFileName = multipartFile.getOriginalFilename();	
+			vo.setFileName(uploadFileName);
+			vo.setUploadPath(datePath);
+			
+			/* uuid 적용 파일 이름 */
+			String uuid = UUID.randomUUID().toString();
+			vo.setUuid(uuid);
+
+			
+			uploadFileName = uuid + "_" + uploadFileName;
+			
+			/* 파일 위치, 파일 이름을 합친 File 객체 */
+			File saveFile = new File(uploadPath, uploadFileName);
+			
+			/* 파일 저장 */
+			try {
+				multipartFile.transferTo(saveFile);
+//				
+//				File thumbnailFile = new File(uploadPath, "s_" + uploadFileName);
+//				
+//				BufferedImage bo_image = ImageIO.read(saveFile);
+//				
+//					/* 비율 */
+//					double ratio = 3;
+//					/*넓이 높이*/
+//					int width = (int) (bo_image.getWidth() / ratio);
+//					int height = (int) (bo_image.getHeight() / ratio);	
+//					
+//				BufferedImage bt_image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+//								
+//				Graphics2D graphic = bt_image.createGraphics();
+//				
+//				graphic.drawImage(bo_image, 0, 0,width,height, null);
+//					
+//				ImageIO.write(bt_image, "jpg", thumbnailFile);
+				
+				/* 방법 2 */
+				File thumbnailFile = new File(uploadPath, "s_" + uploadFileName);	
+				
+				BufferedImage bo_image = ImageIO.read(saveFile);
+
+					//비율 
+					double ratio = 3;
+					//넓이 높이
+					int width = (int) (bo_image.getWidth() / ratio);
+					int height = (int) (bo_image.getHeight() / ratio);					
+				
+				
+				Thumbnails.of(saveFile)
+		        .size(width, height)
+		        .toFile(thumbnailFile);
+				
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+				
+			} 
+			
+			list.add(vo);
+			
+		} //for
+		
+		ResponseEntity<List<AttachImageVO>> result = new ResponseEntity<List<AttachImageVO>>(list, HttpStatus.OK);
+		
+		return result;
+
+
+		
+	}
 }

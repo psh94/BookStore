975072f2fceaefc9dca6748e9db3fe692fa2fe0c
Sanghyeok Park <suran5281@gmail.com>
2022-12-01 13:38:50 +0900

    프로젝트 마무리

---
+++ src/main/java/com/psh/interceptor/CartInterceptor.java
@@ -0,0 +1,29 @@
+package com.psh.interceptor;
+
+import com.psh.model.MemberVO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class CartInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HttpSession session = request.getSession();
+		
+		MemberVO mvo = (MemberVO)session.getAttribute("member");
+		
+		if(mvo == null) {
+			response.sendRedirect("/main");
+			return false;
+		} else {
+			return true;
+		}		
+		
+	}	
+	
+}
